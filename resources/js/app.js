f2bdfa8a660e12923d4fcb96cdd546bedda1256c
Luis Mata <mataluis.matag7@gmail.com>
2022-02-21 05:15:01 -0400

    config elemenet-plus, babel, webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import esES from 'element-plus/es/locale/lang/es'
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
@@ -12,6 +16,7 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .use(ElementPlus, { size: 'small', zIndex: 3000, locale: esES })
             .mixin({ methods: { route } })
             .mount(el);
     },
