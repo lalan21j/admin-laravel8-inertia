fbbf47ddd447b514bb6523f782b042e7357be378
Luis Mata <mataluis.matag7@gmail.com>
2022-02-25 22:37:29 -0400

    lang es

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'stored_information' => 'La información fue almacenada satisfactoriamente',
+    'information_successfully_updated' => 'La información fue actualizada satisfactoriamente',
+    'information_not_stored' => 'Se ha presentado un error.!'
+];
