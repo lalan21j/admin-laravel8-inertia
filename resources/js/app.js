18d9bc8d29f869b66346ca14f4359c5d0df715d2
Luis Mata <mataluis.matag7@gmail.com>
2022-02-27 14:21:28 -0400

    webpack, library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,26 @@
-require('./bootstrap');
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-
+// ElementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import esES from 'element-plus/es/locale/lang/es'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+require('./bootstrap')
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
-            .use(plugin)
-            .use(ElementPlus, { size: 'small', zIndex: 3000, locale: esES })
-            .mixin({ methods: { route } })
-            .mount(el);
-    },
-});
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => require(`./Pages/${name}.vue`),
+  setup ({ el, app, props, plugin }) {
+    return createApp({ render: () => h(app, props) })
+      .use(plugin)
+      .use(ElementPlus, { size: 'small', zIndex: 3000, locale: esES })
+      .mixin({ methods: { route } })
+      .mount(el)
+  }
+})
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#4B5563' })
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path')
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
-const {ElementPlusResolver} = require('unplugin-vue-components/resolvers')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
-    resolve: {
-        alias: {
-            '@': path.resolve('resources/js'),
-        },
-        extensions: ['.js', '.jsx', '.mjs']
+  resolve: {
+    alias: {
+      '@': path.resolve('resources/js')
     },
-    module: {
-        rules: [
-            {
-                test: /\.mjs$/i,
-                resolve: {byDependency: {esm: {fullySpecified: false}}}
-            }
-        ]
-    },
-    plugins: [
-        AutoImport({
-            resolvers: [ElementPlusResolver()]
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()]
-        })
+    extensions: ['.js', '.jsx', '.mjs']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/i,
+        resolve: { byDependency: { esm: { fullySpecified: false } } }
+      }
     ]
+  },
+  plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver()
+      ]
+    })
+  ]
 }
